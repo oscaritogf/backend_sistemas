014882458b08d0a6285fc23dbac4044cddb5fdc4
Franklin Amador <joelamador55@gmail.com>
2024-07-20 22:18:49 -0600

    solucion a departamento, con matricula

--- src/models/Admisiones.js
+++ src/models/Admisiones.js
@@ -100,94 +100,92 @@ static async getCarreras() {
   return data;
 }
 
-  static async getCSV() {
-    const { data, error } = await supabase
-      .from('Admisiones')
-      .select('*');
-  
-    if (error) {
-      console.error('Error al obtener datos de Admisiones', error);
-      throw error;
-    }
-  
-    if (!data || data.length === 0) {
-      throw new Error('No hay datos de Admisiones');
-    }
-  
-    // Obtener datos de la tabla Centros
-    const { data: centrosData, error: centrosError } = await supabase
-      .from('Centro')
-      .select('id_Centro, nombre, codigo');
-  
-    if (centrosError) {
-      console.error('Error al obtener datos de Centros', centrosError);
-      throw new Error('Error al obtener datos de Centros');
-    }
-  
-    // Crear un mapa de id_Centro a codigo y nombre
-    const centrosMap = new Map();
-    const centrocodMap = new Map();
-    centrosData.forEach(centro => {
-      centrosMap.set(centro.id_Centro, centro.nombre);
-      centrocodMap.set(centro.id_Centro, centro.codigo);
-    });
-  
-    // Obtener datos de la tabla Carreras
-    const { data: carrerasData, error: carrerasError } = await supabase
-      .from('Carrera')
-      .select('id_Carrera, nombre, puntajeRequerido');
-  
-    if (carrerasError) {
-      console.error('Error al obtener datos de Carreras', carrerasError);
-      throw new Error('Error al obtener datos de Carreras');
-    }
-  
-    // Crear un mapa de id_Carrera a nombre y puntajeRequerido
-    const carrerasMap = new Map();
-    const puntajeMap = new Map();
-    const deptoMap = new Map();
-    carrerasData.forEach(carrera => {
-      carrerasMap.set(carrera.id_Carrera, carrera.nombre);
-      puntajeMap.set(carrera.id_Carrera, carrera.puntajeRequerido);
-      deptoMap.set(carrera.id_Carrera, carrera.id_Depto);
-    });
-  
-    // Convertir los datos a formato CSV
-    let csv = 'id_Admision,dni,primer_Nombre,segundo_Nombre,primer_Apellido,segundo_Apellido,Centro,Codigo,Carrera,Sd_Carrera,email,intentos,nota1,nota2,aprobacionPAA,aprobacionPAM_PCCNS,matricula,depto\n';
-    data.forEach(admision => {
-      const cent = centrocodMap.get(admision.id_Centro) || '';
-      const codigoCentro = centrosMap.get(admision.id_Centro) || '';
-      const nombreCarrera = carrerasMap.get(admision.id_Carrera) || '';
-      const nombreSdCarrera = carrerasMap.get(admision.id_Sd_Carrera) || '';
-      const deptoCode = deptoMap.get(admision.id_Carrera) || 'ninguno';
-  
-      // Determinar el valor de matricula
-      let matricula = 'ninguna';
-      let depto = 'ninguno';
-      if (admision.nota1 >= puntajeMap.get(admision.id_Carrera)) {
-        matricula = nombreCarrera;
-        depto = deptoCode;
-      } else if (admision.nota1 >= puntajeMap.get(admision.id_Sd_Carrera)) {
-        matricula = nombreSdCarrera;
-        depto = deptoCode
+static async getCSV() {
+  const { data, error } = await supabase
+    .from('Admisiones')
+    .select('*');
 
-      }
-  
-      csv += `${admision.id_Admision},${admision.dni},${admision.primer_Nombre},${admision.segundo_Nombre},${admision.primer_Apellido},${admision.segundo_Apellido},${codigoCentro},${cent},${nombreCarrera},${nombreSdCarrera},${admision.email},${admision.intentos},${admision.nota1},${admision.nota2},${admision.aprobacionPAA},${admision.aprobacionPAM_PCCNS},${matricula},${depto}\n`;
-    });
-  
-    // Guardar el CSV en un archivo
-    try {
-      const filePath = 'Admisiones.csv';
-      await fs.writeFile(filePath, csv);
-      console.log('Archivo CSV de Admisiones creado con éxito.');
-    } catch (error) {
-      console.error('Error al escribir el archivo CSV', error);
+  if (error) {
+    console.error('Error al obtener datos de Admisiones', error);
+    throw error;
+  }
+
+  if (!data || data.length === 0) {
+    throw new Error('No hay datos de Admisiones');
+  }
+
+  // Obtener datos de la tabla Centros
+  const { data: centrosData, error: centrosError } = await supabase
+    .from('Centro')
+    .select('id_Centro, nombre, codigo');
+
+  if (centrosError) {
+    console.error('Error al obtener datos de Centros', centrosError);
+    throw new Error('Error al obtener datos de Centros');
+  }
+
+  // Crear un mapa de id_Centro a codigo y nombre
+  const centrosMap = new Map();
+  const centrocodMap = new Map();
+  centrosData.forEach(centro => {
+    centrosMap.set(centro.id_Centro, centro.nombre);
+    centrocodMap.set(centro.id_Centro, centro.codigo);
+  });
+
+  // Obtener datos de la tabla Carreras
+  const { data: carrerasData, error: carrerasError } = await supabase
+    .from('Carrera')
+    .select('id_Carrera, nombre, puntajeRequerido, id_Depto');
+
+  if (carrerasError) {
+    console.error('Error al obtener datos de Carreras', carrerasError);
+    throw new Error('Error al obtener datos de Carreras');
+  }
+
+  // Crear un mapa de id_Carrera a nombre, puntajeRequerido y id_Depto
+  const carrerasMap = new Map();
+  const puntajeMap = new Map();
+  const deptoMap = new Map();
+  carrerasData.forEach(carrera => {
+    carrerasMap.set(carrera.id_Carrera, carrera.nombre);
+    puntajeMap.set(carrera.id_Carrera, carrera.puntajeRequerido);
+    deptoMap.set(carrera.id_Carrera, carrera.id_Depto);
+  });
+
+  // Convertir los datos a formato CSV
+  let csv = 'id_Admision,dni,primer_Nombre,segundo_Nombre,primer_Apellido,segundo_Apellido,Centro,Codigo,Carrera,Sd_Carrera,email,intentos,nota1,nota2,aprobacionPAA,aprobacionPAM_PCCNS,matricula,depto\n';
+  data.forEach(admision => {
+    const cent = centrocodMap.get(admision.id_Centro) || '';
+    const codigoCentro = centrosMap.get(admision.id_Centro) || '';
+    const nombreCarrera = carrerasMap.get(admision.id_Carrera) || '';
+    const nombreSdCarrera = carrerasMap.get(admision.id_Sd_Carrera) || '';
+
+    // Determinar el valor de matricula y depto
+    let matricula = 'ninguna';
+    let depto = 'ninguno';
+    if (admision.nota1 >= puntajeMap.get(admision.id_Carrera)) {
+      matricula = nombreCarrera;
+      depto = deptoMap.get(admision.id_Carrera) || 'ninguno';
+    } else if (admision.nota1 >= puntajeMap.get(admision.id_Sd_Carrera)) {
+      matricula = nombreSdCarrera;
+      depto = deptoMap.get(admision.id_Sd_Carrera) || 'ninguno';
     }
-    
-    return csv;
+
+    csv += `${admision.id_Admision},${admision.dni},${admision.primer_Nombre},${admision.segundo_Nombre},${admision.primer_Apellido},${admision.segundo_Apellido},${codigoCentro},${cent},${nombreCarrera},${nombreSdCarrera},${admision.email},${admision.intentos},${admision.nota1},${admision.nota2},${admision.aprobacionPAA},${admision.aprobacionPAM_PCCNS},${matricula},${depto}\n`;
+  });
+
+  // Guardar el CSV en un archivo
+  try {
+    const filePath = 'Admisiones.csv';
+    await fs.writeFile(filePath, csv);
+    console.log('Archivo CSV de Admisiones creado con éxito.');
+  } catch (error) {
+    console.error('Error al escribir el archivo CSV', error);
   }
   
+  return csv;
+}
+
 
   static async readCsv(csvFilePath) {
     
