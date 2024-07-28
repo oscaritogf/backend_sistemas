3e540ac6d498e78242ad19bbc7b4e3d134a969f0
oscaritogf <oigarciaf@unah.hn>
2024-07-28 15:51:47 -0600

    listar lista espera funcional

--- src/routes/matricula.js
+++ src/routes/matricula.js
@@ -9,4 +9,19 @@ router.get('/asignaturas/:codigo/secciones', matriculaController.getSeccionesByA
 router.get('/estudiantes/:id_estudiante/asignaturas', matriculaController.getAsignaturasEstudiante);
 router.post('/matricular', matriculaController.matricular);
 
+// Ruta para cancelar matrícula
+router.post('/cancelar', matriculaController.cancelarMatricula);
+
+// Ruta para listar asignaturas matriculadas
+router.get('/estudiantes/:id_estudiante/asignatura', matriculaController.listarAsignaturasMatriculadas);
+
+// Ruta para actualizar cupos
+router.put('/secciones/:id_seccion/cupos', matriculaController.actualizarCupos);
+
+
+///listar las lista de espera
+router.get('/lista-espera/seccion/:id_seccion', matriculaController.listarEstudiantesEnEspera);
+router.get('/lista-espera/estudiante/:id_estudiante', matriculaController.listarClasesEnEspera);
+
+
 module.exports = router;
