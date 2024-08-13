9e158ae1fa35f8195faab1541502ff0112e5fe9c
Franklin Amador <joelamador55@gmail.com>
2024-08-12 18:02:49 -0600

    correcion lista alumnos

--- src/routes/teacher.js
+++ src/routes/teacher.js
@@ -6,7 +6,8 @@ const { checkRole } = require('../middleware/roleCheck');
 
 // router.get('/data', authenticateToken, checkRole('docente'), teacherController.getData);
 router.post('/secciones', teacherController.getSecciones);
-router.post('/estudiantes', teacherController.getStudents);
+router.get('/estudiantes/:seccion', teacherController.getStudentsExcel);
+router.get('/students/:Seccion', teacherController.getStudents);
 
 router.post('/fcourse', teacherController.finishCourse);
 router.post('/notas', teacherController.uploadNotes);
